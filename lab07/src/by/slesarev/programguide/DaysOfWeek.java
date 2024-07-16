c73a77d01cd9e4721acb74f2b23d3ff4861731e3
Vanya Slesarev <119366827+shashlyk55@users.noreply.github.com>
2024-07-16 12:27:37 +0300

    Add files via upload

---
+++ lab07/src/by/slesarev/programguide/DaysOfWeek.java
@@ -0,0 +1,11 @@
+package by.slesarev.programguide;
+
+public enum DaysOfWeek {
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY
+}
---
+++ lab07/src/by/slesarev/tvprogram/ITVProgram.java
@@ -0,0 +1,5 @@
+package by.slesarev.tvprogram;
+
+public interface ITVProgram {
+    void ShowProgram();
+}
